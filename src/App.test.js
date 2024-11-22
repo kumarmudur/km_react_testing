d9d17814e91b77a5e688f32ec144e5a268fe33a3
kumarmudur <kumarmudur10@gmail.com>
2024-11-21 21:30:56 -0500

    test case updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("first react app", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText("First React Test Case");
+  expect(text).toBeInTheDocument();
+});
+
+test("testing input box", () => {
+  render(<App />);
+  let checkInput = screen.getByRole("textbox");
+  let checkPlaceholder = screen.getByPlaceholderText("Enter User Name");
+  expect(checkInput).toBeInTheDocument();
+  expect(checkPlaceholder).toBeInTheDocument();
+  expect(checkInput).toHaveAttribute("name", "username");
+  expect(checkInput).toHaveAttribute("id", "userId");
+  expect(checkInput).toHaveAttribute("value", "shiva");
+});
+
+describe("UI test case group", () => {
+  test("case 1", () => {
+    render(<App />);
+    let checkInput = screen.getByRole("textbox");
+    expect(checkInput).toBeInTheDocument();
+  });
 });
