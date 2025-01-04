ad52cf75ae1040aef22334a7fda8d15049790db3
kumarmudur <kumarmudur10@gmail.com>
2025-01-04 07:36:08 -0500

    [Reactjs] app test changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+// test("snapshot testing for app component", () => {
+//   const view = render(<App />);
+//   expect(view).toMatchSnapshot();
+// });
+
 test("first react app", () => {
   render(<App />);
   const text = screen.getByText("First React Test Case");
   expect(text).toBeInTheDocument();
-});
-
-test("testing input box", () => {
-  render(<App />);
-  let checkInput = screen.getByRole("textbox");
-  let checkPlaceholder = screen.getByPlaceholderText("Enter User Name");
-  expect(checkInput).toBeInTheDocument();
-  expect(checkPlaceholder).toBeInTheDocument();
-  expect(checkInput).toHaveAttribute("name", "username");
-  expect(checkInput).toHaveAttribute("id", "userId");
-  expect(checkInput).toHaveAttribute("value", "shiva");
-});
-
-describe("UI test case group", () => {
-  test("case 1", () => {
-    render(<App />);
-    let checkInput = screen.getByRole("textbox");
-    expect(checkInput).toBeInTheDocument();
-  });
 });
